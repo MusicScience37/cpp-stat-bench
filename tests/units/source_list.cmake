556d64d106330097d5f559be4e2348e8fcf419b1
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-12-19 18:40:19 +0900

    :sparkles: Add MonotoneClock.

--- tests/units/source_list.cmake
+++ tests/units/source_list.cmake
@@ -1,5 +1,6 @@
 set(SOURCE_FILES
     stat_bench/clock/duration_test.cpp
     stat_bench/clock/monotone_clock_impl_test.cpp
+    stat_bench/clock/monotone_clock_test.cpp
     stat_bench/clock/monotone_time_point_test.cpp
 )
