df44750e76e8669ac2917ddf3b2c56dbee3aefe5
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-12-25 20:28:55 +0900

    :sparkles: Add fixtures.

---
+++ include/stat_bench/bench/fixture_base.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2021 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of FixtureBase class.
+ */
+#pragma once
+
+#include "stat_bench/bench/i_benchmark_case.h"
+
+namespace stat_bench {
+namespace bench {
+
+/*!
+ * \brief Base class of fixtures.
+ */
+class FixtureBase : public IBenchmarkCase {
+public:
+    /*!
+     * \brief Setup before running actual process.
+     *
+     * \note This can be implemented in fixtures to perform common
+     * initialization process.
+     *
+     * \param[in] context Context.
+     */
+    virtual void setup(InvocationContext& context) {
+        // no operation in default implementation
+    }
+
+    /*!
+     * \brief Tear down after running actual process.
+     *
+     * \note This can be implemented in fixtures to perform common
+     * finalization process.
+     * \note This will be called even when errors occurred in run function.
+     *
+     * \param[in] context Context.
+     */
+    virtual void tear_down(InvocationContext& context) {
+        // no operation in default implementation
+    }
+
+    /*!
+     * \brief Run actual process in each case.
+     *
+     * \note This will be implemented in each case, so don't implement in
+     * fixtures.
+     *
+     * \param[in] context Context.
+     */
+    virtual void run(InvocationContext& context) = 0;
+
+    FixtureBase(const FixtureBase&) = delete;
+    FixtureBase(FixtureBase&&) = delete;
+    auto operator=(const FixtureBase&) -> FixtureBase& = delete;
+    auto operator=(FixtureBase&&) -> FixtureBase& = delete;
+
+    /*!
+     * \brief Destructor.
+     */
+    ~FixtureBase() override = default;
+
+protected:
+    /*!
+     * \brief Constructor.
+     */
+    FixtureBase() = default;
+
+private:
+    //! \copydoc stat_bench::bench::IBenchmarkCase::execute
+    void execute(InvocationContext& context) final {
+        setup(context);
+        try {
+            run(context);
+            tear_down(context);
+        } catch (...) {
+            tear_down(context);
+            throw;
+        }
+    }
+};
+
+}  // namespace bench
+}  // namespace stat_bench
---
+++ tests/units/stat_bench/bench/fixture_base_test.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Test of FixtureBase class.
+ */
+#include "stat_bench/bench/fixture_base.h"
+
+#include <stdexcept>
+
+#include <catch2/catch_test_macros.hpp>
+#include <trompeloeil.hpp>
+
+#include "mock_fixture.h"
+#include "stat_bench/bench/i_benchmark_case.h"
+
+TEST_CASE("stat_bench::bench::FixtureBase") {
+    stat_bench_test::bench::MockFixture fixture;
+
+    stat_bench::bench::IBenchmarkCase& fixture_as_case = fixture;
+
+    constexpr std::size_t threads = 1;
+    constexpr std::size_t iterations = 7;
+    constexpr std::size_t samples = 13;
+
+    stat_bench::bench::InvocationContext context{
+        stat_bench::bench::BenchmarkCondition(threads), iterations, samples};
+
+    SECTION("execute") {
+        trompeloeil::sequence seq;
+        REQUIRE_CALL(fixture, setup(trompeloeil::_)).TIMES(1).IN_SEQUENCE(seq);
+        REQUIRE_CALL(fixture, run(trompeloeil::_)).TIMES(1).IN_SEQUENCE(seq);
+        REQUIRE_CALL(fixture, tear_down(trompeloeil::_))
+            .TIMES(1)
+            .IN_SEQUENCE(seq);
+
+        REQUIRE_NOTHROW(fixture_as_case.execute(context));
+    }
+
+    SECTION("execute to throw exception") {
+        trompeloeil::sequence seq;
+        REQUIRE_CALL(fixture, setup(trompeloeil::_)).TIMES(1).IN_SEQUENCE(seq);
+        REQUIRE_CALL(fixture, run(trompeloeil::_))
+            .TIMES(1)
+            .IN_SEQUENCE(seq)
+            // NOLINTNEXTLINE
+            .THROW(std::invalid_argument("Test exception"));
+        REQUIRE_CALL(fixture, tear_down(trompeloeil::_))
+            .TIMES(1)
+            .IN_SEQUENCE(seq);
+
+        REQUIRE_THROWS_AS(
+            fixture_as_case.execute(context), std::invalid_argument);
+    }
+}
