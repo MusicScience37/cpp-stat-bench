67ac478318435f33f95ef1b0601853f19783a14b
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-12-25 18:10:47 +0900

    :sparkles: Add a class to measure using threads.

--- tests/units/source_list.cmake
+++ tests/units/source_list.cmake
@@ -4,6 +4,7 @@ set(SOURCE_FILES
     stat_bench/bench/benchmark_condition_test.cpp
     stat_bench/bench/benchmark_group_test.cpp
     stat_bench/bench/invocation_context_test.cpp
+    stat_bench/bench/threadable_invoker_test.cpp
     stat_bench/clock/duration_test.cpp
     stat_bench/clock/monotone_clock_impl_test.cpp
     stat_bench/clock/monotone_clock_test.cpp
