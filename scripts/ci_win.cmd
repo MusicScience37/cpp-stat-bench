b9afb4468baef9f7cae3695c47cf9db7210dc4f6
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-11-30 13:25:29 +0000

    :wrench: Try to use ccache in CI on Windows.

---
+++ scripts/ci_win.cmd
@@ -0,0 +1,17 @@
+call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\VC\\Auxiliary\\Build\\vcvarsall.bat" x86_x64
+
+cmake .. ^
+    -G Ninja ^
+    -DCMAKE_BUILD_CXX_COMPILER_LAUNCHER=ccache ^
+    -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DSTAT_BENCH_TESTING:BOOL=ON ^
+    -DSTAT_BENCH_ENABLE_BENCH=ON ^
+    -DSTAT_BENCH_TEST_BENCHMARKS=ON ^
+    -DSTAT_BENCH_BUILD_EXAMPLES=ON ^
+    -DSTAT_BENCH_TEST_EXAMPLES=ON ^
+    -DSTAT_BENCH_WRITE_JUNIT:BOOL=ON
+
+cmake --build . --config Release --parallel
+
+ctest -V --build-config Release
