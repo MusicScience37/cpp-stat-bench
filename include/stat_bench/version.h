565660c5fd4f97cceef7c4d8166f506b1f41a6b7
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-01-04 09:02:43 +0000

    Prepare for release v0.19.0.

--- include/stat_bench/version.h
+++ include/stat_bench/version.h
@@ -23,7 +23,7 @@
 #define STAT_BENCH_VERSION_MAJOR 0
 
 //! Minor version of cpp-stat-bench project.
-#define STAT_BENCH_VERSION_MINOR 18
+#define STAT_BENCH_VERSION_MINOR 19
 
 //! Patch version of cpp-stat-bench project.
 #define STAT_BENCH_VERSION_PATCH 0
