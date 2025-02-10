d3e731991dd5dcffbc6d3e9dbdc46828dee4b750
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-02-10 03:00:17 +0000

    :rotating_light: Fix linter warnings in tests/integ directory.

--- tests/integ/conftest.py
+++ tests/integ/conftest.py
@@ -13,6 +13,7 @@ from .bench_executor import BenchExecutor
 
 
 def pytest_addoption(parser: pytest.Parser) -> None:
+    """Add options to pytest."""
     parser.addoption(
         "--build", action="store", required=True, help="Build directory of CMake."
     )
@@ -21,17 +22,18 @@ def pytest_addoption(parser: pytest.Parser) -> None:
 @pytest.fixture(scope="session", autouse=True)
 def configure_approvaltests():
     """Configure approvaltests library."""
-
     approvaltests.set_default_reporter(ReporterThatAutomaticallyApproves())
 
 
 @pytest.fixture
 def build_dir(pytestconfig: pytest.Config) -> pathlib.Path:
+    """Get the build directory."""
     return pathlib.Path(str(pytestconfig.getoption("build"))).absolute()
 
 
 @pytest.fixture
 def temp_test_dir(build_dir: pathlib.Path) -> pathlib.Path:
+    """Get the temporary directory for tests."""
     return build_dir / "temp_test"
 
 
@@ -39,34 +41,41 @@ def temp_test_dir(build_dir: pathlib.Path) -> pathlib.Path:
 def bench_executor(
     request: pytest.FixtureRequest, temp_test_dir: pathlib.Path
 ) -> BenchExecutor:
+    """Get the executor of benchmarks."""
     return BenchExecutor(request=request, temp_test_dir=temp_test_dir)
 
 
 @pytest.fixture
 def bin_dir(build_dir: pathlib.Path) -> pathlib.Path:
+    """Get the bin directory."""
     return build_dir / "bin"
 
 
 @pytest.fixture
 def simple_benchmark(bin_dir: pathlib.Path) -> pathlib.Path:
+    """Get the path of executable stat_bench_test_integ_simple_benchmark."""
     return bin_dir / "stat_bench_test_integ_simple_benchmark"
 
 
 @pytest.fixture
 def groups_and_cases(bin_dir: pathlib.Path) -> pathlib.Path:
+    """Get the path of executable stat_bench_test_integ_groups_and_cases."""
     return bin_dir / "stat_bench_test_integ_groups_and_cases"
 
 
 @pytest.fixture
 def error_in_benchmark(bin_dir: pathlib.Path) -> pathlib.Path:
+    """Get the path of executable stat_bench_test_integ_error_in_benchmark."""
     return bin_dir / "stat_bench_test_integ_error_in_benchmark"
 
 
 @pytest.fixture
 def parametrized_benchmark(bin_dir: pathlib.Path) -> pathlib.Path:
+    """Get the path of executable stat_bench_test_integ_parametrized_benchmark."""
     return bin_dir / "stat_bench_test_integ_parametrized_benchmark"
 
 
 @pytest.fixture
 def duplicate_cases(bin_dir: pathlib.Path) -> pathlib.Path:
+    """Get the path of executable stat_bench_test_integ_duplicate_cases."""
     return bin_dir / "stat_bench_test_integ_duplicate_cases"
